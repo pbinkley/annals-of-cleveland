2a0c04ca14a8da6c2a627ee8db2a4fd036cc35e5
Peter Binkley <peter.binkley@ualberta.ca>
2019-10-17 17:41:28 -0600

    Improve and extend parsing; output json

--- process.rb
+++ process.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'nokogiri'
-require 'csv'
+require 'json'
+require 'date'
 require 'byebug'
 
 months = {
@@ -18,42 +19,168 @@ months = {
   'Nov.' => 11,
   'Dec.' => 12
 }
+year = 1864
+volume = '47-1'
+newspapers = {
+  'L': {title: 'Cleveland Morning Leader', chronam: 'https://chroniclingamerica.loc.gov/lccn/sn83035143/'}
+}
+
+# page-level url: https://chroniclingamerica.loc.gov/lccn/sn83035143/1864-01-01/ed-1/seq-2/
 
 doc = File.open("source/view-source_https___babel.hathitrust.org_cgi_ssd_id=iau.31858046133199#seq109.html") { |f| Nokogiri::HTML(f) }
 
 pages = doc.xpath('//div[@class="Page"]')
-entries = []
+entries = {}
 prev = 0
+preventry = nil
+linebuffer = []
+in_header = false
+heading = ''
+subheading = ''
+breaks = 0
+highest = 0
 
-pages.each do |page|
+# abstracts pages
+pages[24..384].each do |page|
+  in_header = true
   seq = page.xpath('./@id').first.text
   lines = page.xpath('./p[@class="Text"]').first.text.split(/\n+/)
   lines.each_with_index do |line, index|
-    metadata = line.match(/^(\d+) [-–] L\.? ((?:Jan.|Feb.|Mar.|Apr.|May|June|July|Aug.|Sept.|Oct.|Nov.|Dec.)) (\d+)[;:,]+\ ?([a-zA-Z]*):?\ ?(\d+)\/(\d+)(.*)$/)
+    stripped_line = line.gsub(/^[ -]*/, '').gsub(/[ -]*$/, '')
+    next if stripped_line == ''
+    
+    # detect and ignore page headers
+    if in_header
+      unless
+        stripped_line.match(/^\d*$/) or
+        stripped_line.match(/^CLEVELAND NEWSPAPER DIGEST.*/) or
+        stripped_line.match(/^Abstracts \d.*/) or
+        stripped_line.match(/.*\(Co[nr]t'd\)( -)?/)
+        in_header = false
+      end
+    end
+    next if in_header
+
+    # detect headings, see, see also
+    is_heading = false
+    # ignore dashes at end of line
+    if stripped_line.match(/^[A-Z\&\ ]*$/)
+      heading = stripped_line
+      subheading = ''
+      is_heading = true
+    end
+    if stripped_line.match(/^[A-Z\&\ ]*\. See .*$/)
+      # TODO handle see reference
+      is_heading = true
+    end
+    if stripped_line.match(/^See also .*$/)
+      # TODO handle see also reference
+      is_heading = true
+    end
+    if linebuffer.count > 0
+      if linebuffer.last.match(/.*\((\d+)\)$/) and stripped_line.match(/^[A-Z].*/) and !(is_heading)
+        subheading = stripped_line
+        is_heading = true
+      end
+    end
+    next if is_heading
+    
+    # parse line to find start-of-item metadata
+    metadata = line.match(/^(\d+) [-–] ([a-zA-Z]+)[\.,]? ((?:Jan.|Feb.|Mar.|Apr.|May|June|July|Aug.|Sept.|Oct.|Nov.|Dec.)) (\d+)[;:,]+\ ?([a-zA-Z]*)[;:,]?\ ?(\d+)\/(\d+)(.*)$/)
+    linebuffer << line unless metadata
     next unless metadata
+    if preventry
+      preventry[:lines] = linebuffer
+      inches = preventry[:lines].last.match(/.*\((\d+)\)$/)
+      if inches
+        preventry[:inches] = inches[1].to_i
+      else
+        preventry[:inches] = 0
+      end
+      linebuffer = [line]
+    end
     record = {
       id: metadata[1].to_i,
       seq: seq,
       line: index,
-      month: months[metadata[2]],
-      day: metadata[3].to_i,
-      page: metadata[5].to_i,
-      column: metadata[6].to_i,
-      type: metadata[4],
-      init: metadata[7]
+      newspaper: metadata[2].to_sym,
+      month: months[metadata[3]],
+      day: metadata[4].to_i,
+      page: metadata[6].to_i,
+      column: metadata[7].to_i,
+      type: metadata[5],
+      init: metadata[8],
+      heading: heading,
+      subheading: subheading,
+      terms: []
     }
-    entries << record
-    if record[:id] != prev + 1
-      puts 'break'
-    end
-    puts record[:id].to_s
+    entries[record[:id]] = record
+    
+    highest = record[:id] if record[:id] > highest
+    breaks += 1 if record[:id] != prev + 1
     prev = record[:id]
+    preventry = record
   end
 end
 
-CSV.open("data.csv", "wb") do |csv|
-  csv << entries.first.keys # adds the attributes name on the first line
-  entries.each do |hash|
-    csv << hash.values
+puts 'Breaks: ' + breaks.to_s
+puts 'Entries: ' + entries.keys.count.to_s
+puts 'Highest: ' + highest.to_s
+# index pages
+pages[398..465].each do |page|
+  seq = page.xpath('./@id').first.text
+  lines = page.xpath('./p[@class="Text"]').first.text.split(/\n+/)
+  lines.each_with_index do |line, index|
+    elements = line.match(/^(.*)\, ([0-9\ ]*)$/)
+    next unless elements
+    term = elements[1]
+    ids = elements[2].split
+    ids.each do |id|
+      entries[id.to_i] = {id: id.to_i, terms: []} unless entries[id.to_i]
+      entries[id.to_i][:terms] << term
+    end
   end
 end
+
+File.open("data.json","w") do |f|
+  f.puts JSON.pretty_generate(entries)
+end
+
+File.open("data.html","w") do |f|
+  entries.keys.each do |key|
+    entry = entries[key]
+    next unless entry[:month]
+    date = Date.new(year, entry[:month], entry[:day])
+    newspaper = newspapers[entry[:newspaper]]
+    f.puts "<div class='entry'>"
+    f.puts "<h3>#{volume}.#{entry[:id]}</h3>"
+    if newspaper
+      f.puts "<p><a href='#{newspaper[:chronam]}#{date.to_s}/ed-1/seq-#{entry[:page]}'>#{newspaper[:title]}, #{date.strftime('%e %B %Y')}, p.#{entry[:page]}</a>, col.#{entry[:column]} #{entry[:type]} (#{entry[:inches].to_s} inches)</p>"
+    else
+      f.puts "<p>#{entry[:newspaper]}, #{date.strftime('%e %B %Y')}, p.#{entry[:page]}, col.#{entry[:column]} #{entry[:type]}</p>"
+    end
+    if entry[:lines]
+      f.puts "<p>"
+      entry[:lines].each {|line| f.puts "#{line}<br/>"}
+      f.puts "</p>"
+    end
+    f.puts"<ul>"
+    f.puts "<li>#{entry[:heading]}#{entry[:subheading] != '' ? ' - ' + entry[:subheading] : ''}</li>"
+    entry[:terms].each do |term|
+      f.puts "<li>#{term}</li>"
+    end
+    f.puts"</ul>"
+  end
+end
+
+missing = 0
+File.open("missing.txt","w") do |f|
+  (1..highest).each do |key|
+    if !entries.keys.include?(key) or !entries[key][:month]
+      f.puts key.to_s
+      missing += 1
+    end
+  end
+end
+
+puts 'Missing: ' + missing.to_s
