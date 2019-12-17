f56de2d86c123d748eed0a7be62fec5b0c396e02
Peter Binkley <peter.binkley@ualberta.ca>
2019-12-17 11:55:24 -0700

    Refactor source text, entries and headings parsing into lib/sourcetext.rb

--- lib/utils.rb
+++ lib/utils.rb
@@ -8,3 +8,28 @@ def convert_OCR_number(number)
   number.gsub('O', '0').gsub(/[lI!T]/, '1').gsub(/[GS]/, '5').to_i
 end
 
+def report_list(list, name)
+  lastNumber = 0
+  missingNumbers = []
+  disorderedNumbers = []
+  list.each do |n|
+    if n <= lastNumber
+      # out of order
+      disorderedNumbers << n
+    elsif n != lastNumber + 1
+      missingNumbers +=  (lastNumber.to_i + 1 .. n.to_i - 1).to_a
+    end
+    lastNumber = n
+  end
+  if missingNumbers.empty?
+    puts "No missing #{name} numbers"
+  else
+    puts "Missing #{name} numbers: #{missingNumbers.map { |p| p.to_s }.join(' ')}"
+  end
+  if disorderedNumbers.empty?
+    puts "No disordered #{name} numbers"
+  else
+    puts "Disordered #{name} numbers: #{disorderedNumbers.map { |p| p.to_s }.join(' ')}"
+  end
+end
+
